d63a84506df733d9ccd3010b2bb9d70952363043
Lawson <lawsonmcphetridge2020@gmail.com>
2023-01-13 11:11:30 -0800

    passed changing of candidate body test to pass

--- src/components/reducers/shopping-list-item-reducer.test.js
+++ src/components/reducers/shopping-list-item-reducer.test.js
@@ -1,11 +1,28 @@
-import { shoppingListLoadStartAction } from '../actions/shopping-list-actions';
+import { shoppingListCandidateBodyChanged, shoppingListLoadStartAction } from '../actions/shopping-list-actions';
 import { reducer, initialState } from './shopping-list-reducer';
 
 describe('shopping-list-item-reducer', () => {
   describe('post list', () => {
     it('makes loading mode change to loading', () => {
-      const newState = reducer(initialState(), shoppingListLoadStartAction());
+      const newState = reducer(
+        initialState(),
+        shoppingListLoadStartAction()
+      );
       expect(newState.loadingMode).toEqual('Loading');
+    });
+  });
+
+  describe('post candidate', () => {
+    it('uses new body provided', () => {
+      const oldState = {
+        ...initialState(),
+        shoppingCandidateBody: 'hello',
+      };
+      const newState = reducer(
+        oldState,
+        shoppingListCandidateBodyChanged('hello')
+      );
+      expect(newState.shoppingCandidateBody).toEqual('hello');
     });
   });
 });
